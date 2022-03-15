11810a084423a3d51124d89fd6f6e2e914761f66
§T4Rk~9000 <stark9000@gmail.com>
2022-03-15 21:50:24 +0530

    Add files via upload

---
+++ rf_servo_rx_1/rf_servo_rx_1.ino
@@ -0,0 +1,45 @@
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <Servo.h>
+
+int CE_PIN = 8;
+int CSN_PIN = 10;
+
+RF24 Radio (CE_PIN, CSN_PIN);
+
+const byte address[6] = "00001";
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+int i = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("started!");
+  Radio.begin();
+  Radio.openReadingPipe(0, address);
+  Radio.setPALevel(RF24_PA_MIN);
+  Radio.startListening();
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+}
+
+void loop() {
+  if (Radio.available()) {
+    char text[32] = "";
+    Radio.read(&text, sizeof(text));
+    Serial.println(text);
+    if (text == "M1") {
+      for (i = 0; i < 90; i++) {
+        servo1.write(i);
+        delay(10);
+      }
+    }
+  }
+}
